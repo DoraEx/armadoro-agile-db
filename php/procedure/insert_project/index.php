8c3d556efc88b963f7fffa5cafc20ba1a88173e9
mandy1339 <mandy1339@gmail.com>
2017-12-14 14:08:04 -0500

    automated script commit

--- php/procedure/insert_project/index.php
+++ php/procedure/insert_project/index.php
@@ -5,21 +5,21 @@ and set it's read state to 1 -->
 <?php require(realpath($_SERVER["DOCUMENT_ROOT"]) . '/service/session.php'); ?>
 
 <?php
-error_log("\nIn Insert Project File\n\n", 3, "/home/armando/php_log.log");
+
 
 $e_id         = $_SESSION['e_id'];
 $project_name = htmlentities($_POST['project_name']);
 $date_due     = htmlentities($_POST['date_due']);
 $date_start   = 'NOW()';
 
-error_log("\n . $project_name . \n\n", 3, "/home/armando/php_log.log");
-error_log("\n . $date_due . \n\n", 3, "/home/armando/php_log.log");
+
+
 
 if ($e_id != null && $project_name != null) {
-    error_log("\nABOUT TO RUN PROCEDURE\n\n", 3, "/home/armando/php_log.log");
+
     $insert_query = "CALL insert_project_proc($e_id, \"$project_name\", \"$date_due\")";
     $result = db_query($insert_query);
-    error_log((string)$result . " is the result" . "\n\n", 3, "/home/armando/php_log.log");
+
 }
 ?>
 
